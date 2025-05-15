da6a6cd4a7f209826c01e3e00f8e532c5cba5a1b
bash360 <mark.bashir.ugwi@gmail.com>
2025-05-15 14:01:08 +0100

    added shell script

---
+++ build.sh
@@ -0,0 +1,21 @@
+set -e 
+#!/bin/bash
+STACK="main"
+PULUMI_DIR="./pulumi"
+ANSIBLE_DIR="./ansible"
+
+
+function createServer(){
+echo "Pulumi stack $Stack"
+echo "Provisioning infrastructure..."
+
+
+cd $PULUMI_DIR
+pulumi stack select $STACK
+pulumi up --yes
+}
+
+createServer
+
+
+
---
+++ destroy.sh
@@ -0,0 +1,47 @@
+set -e 
+#!/bin/bash
+
+
+
+STACK="main"
+PULUMI_DIR="./pulumi"
+
+function destroyServer(){
+read -p "About to destroy server on main Enter Y/N: " ANSWER
+case "$ANSWER"  in 
+   ([yY] | [yY][eE][sE])
+    echo "Pulumi stack $Stack"
+    echo "Deleting ec2"
+    cd $PULUMI_DIR
+    pulumi stack select $STACK
+    pulumi destroy --yes
+    exit 0
+       ;;
+
+    ([nN] | [nN][oO])
+
+        exit 0
+        ;;
+    *)
+        echo "Please enter y/yes or n/no"
+        ;;
+
+esac
+   
+}
+
+echo "this program will destroy your ec2 instance"
+ read -p "Type delete to proceed: " ANSWER
+
+ if [ "$ANSWER" == "delete" ] 
+then 
+
+   destroyServer
+
+else 
+  echo "operation cancelled"
+  exit 0 
+
+fi
+
+
